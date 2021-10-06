18c61174dd85f1fd63fd10d143a655e031dec1c3
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-10-06 21:06:06 +0900

    fix: センサーデータを取得する関数を分離

--- src/sensor_graph.py
+++ src/sensor_graph.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 import pathlib
 import os
-from influxdb import InfluxDBClient
 import datetime
-import dateutil.parser
 import io
 import matplotlib
 import pprint
@@ -18,34 +16,9 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 from matplotlib.font_manager import FontProperties
 
-INFLUXDB_ADDR = '192.168.0.10'
-INFLUXDB_PORT = 8086
-INFLUXDB_DB = 'sensor'
-
-INFLUXDB_QUERY = """
-SELECT mean("{param}") FROM "sensor.{sensor_type}" WHERE ("hostname" = \'{hostname}\') AND time >= now() - 60h GROUP BY time(3m) fill(previous) ORDER by time asc
-"""
+from sensor_data import fetch_data
 
 IMAGE_DPI = 100.0
-
-def fetch_data(sensor_type, hostname, param):
-    client = InfluxDBClient(
-        host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB
-    )
-    result = client.query(INFLUXDB_QUERY.format(
-        sensor_type=sensor_type, hostname=hostname, param=param)
-    )
-
-    data = list(map(lambda x: x['mean'], result.get_points()))
-
-    localtime_offset = datetime.timedelta(hours=9)
-    time = list(map(lambda x: dateutil.parser.parse(x['time'])+localtime_offset, result.get_points()))
-
-    return {
-        'value': data,
-        'time': time,
-        'valid': len(time) != 0
-    }
 
 
 def get_plot_font(config, font_type, size):
