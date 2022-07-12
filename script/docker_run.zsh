1a2c9191d9df1c1aedb7ec266da0b34b7ddbb74b
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-07-12 22:21:11 +0900

    fix: コードを整理

--- script/docker_run.zsh
+++ script/docker_run.zsh
@@ -7,4 +7,6 @@ set -e
 cd $(dirname $(dirname $0))
 
 docker build --quiet . -t ${APP_NAME}
-docker run --rm ${APP_NAME}
+docker run \
+       --name ${APP_NAME}-$(head -c 10 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | cut -c 1-4) \
+       --rm ${APP_NAME}
