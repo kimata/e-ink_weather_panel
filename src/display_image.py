8d480cb56ff29449071d925cd09a9b9536f9143e
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-07-17 14:08:26 +0900

    fix: Raspberry Pi のメモリリークを防止

--- src/display_image.py
+++ src/display_image.py
@@ -7,7 +7,6 @@ import subprocess
 import time
 import sys
 import os
-import gc
 
 UPDATE_SEC = 120
 
@@ -42,36 +41,31 @@ else:
 
 print("Raspberry Pi hostname: %s" % (rasp_hostname))
 
-ssh = ssh_connect(rasp_hostname, key_filename)
-
 while True:
-    ssh_stdin = None
-    try:
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-            "cat - > /dev/shm/display.png && sudo fbi -1 -T 1 -d /dev/fb0 --noverbose /dev/shm/display.png; echo $?"
+    ssh = ssh_connect(rasp_hostname, key_filename)
+
+    ssh_stdin = ssh.exec_command(
+        "cat - > /dev/shm/display.png && sudo fbi -1 -T 1 -d /dev/fb0 --noverbose /dev/shm/display.png; echo $?"
+    )[0]
+
+    proc = subprocess.Popen(["python3", CREATE_IMAGE], stdout=subprocess.PIPE)
+    ssh_stdin.write(proc.communicate()[0])
+    proc.wait()
+    ssh_stdin.close()
+
+    print(
+        "{time}".format(
+            time=datetime.datetime.now(),
         )
+    )
+    sys.stdout.flush()
 
-        proc = subprocess.Popen(["python3", CREATE_IMAGE], stdout=subprocess.PIPE)
-        ssh_stdin.write(proc.communicate()[0])
-        ssh_stdin.close()
-
-        ret_code = int(ssh_stdout.read().decode("utf-8").strip())
-        if ret_code != 0:
-            raise RuntimeError(ssh_stderr.read().decode("utf-8"))
-
-        print(".", end="")
-        sys.stdout.flush()
-    except:
-        import traceback
-
-        print("")
-        print(traceback.format_exc(), file=sys.stderr)
-        sys.stdout.flush()
-
-        break
-
-    # close だけだと，SSH 側がしばらく待っていることがあったので，念のため
-    del ssh_stdin
-    gc.collect()
-
+    # 更新されていることが直感的に理解しやすくなるように，更新タイミングを 0 秒
+    # に合わせる
+    # (例えば，1分間隔更新だとして，1分40秒に更新されると，2分40秒まで更新されないので
+    # 2分45秒くらいに表示を見た人は本当に1分間隔で更新されているのか心配になる)
     time.sleep(UPDATE_SEC - datetime.datetime.now().second)
+
+    # NOTE: fbi コマンドのプロセスが残るので強制終了させる
+    ssh.exec_command("sudo killall -9 fbi")
+    ssh.close()
