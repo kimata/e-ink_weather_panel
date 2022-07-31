6d4ab8dd66da78d77bf5588976277391531a67f9
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-07-31 16:12:19 +0900

    fix: イメージのビルドを行うようにした

--- script/registry_push.zsh
+++ script/registry_push.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
-REGISTRY=registry.green-rabbit.net
-NAME=library/e-ink_weather_panel
+NAME=e-ink_weather_panel
+REGISTRY=registry.green-rabbit.net/library
 
 git push
+docker build . -t ${NAME}
 docker tag e-ink_weather_panel ${REGISTRY}/${NAME}
 docker push ${REGISTRY}/${NAME}
