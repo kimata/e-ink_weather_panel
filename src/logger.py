2cfe87c7a7646acf21b77969f04687d40a6bd589
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-27 23:32:40 +0900

    fix: コードを整理

--- src/logger.py
+++ src/logger.py
@@ -4,8 +4,12 @@ import coloredlogs
 import logging
 import logging.handlers
 import bz2
+import pathlib
 import os
 import io
+
+MAX_SIZE = 10 * 1024 * 1024
+ROTATE_COUNT = 10
 
 LOG_FORMAT = "{name} %(asctime)s %(levelname)s [%(filename)s:%(lineno)s %(funcName)s] %(message)s"
 
@@ -21,8 +25,27 @@ class GZipRotator:
         os.remove(source)
 
 
-def init(name, level=logging.WARNING, is_str=False):
+def init(name, level=logging.WARNING, dir_path=None, is_str=False):
     coloredlogs.install(fmt=LOG_FORMAT.format(name=name), level=level)
+
+    if dir_path is not None:
+        log_path = pathlib.Path(dir_path)
+        log_path.mkdir(exist_ok=True, parents=True)
+
+        logger = logging.getLogger()
+        log_handler = logging.handlers.RotatingFileHandler(
+            str(log_path / "{name}.log".format(name=name)),
+            encoding="utf8",
+            maxBytes=MAX_SIZE,
+            backupCount=ROTATE_COUNT,
+        )
+        log_handler.formatter = logging.Formatter(
+            fmt=LOG_FORMAT.format(name=name), datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        log_handler.namer = GZipRotator.namer
+        log_handler.rotator = GZipRotator.rotator
+
+        logger.addHandler(log_handler)
 
     if is_str:
         str_io = io.StringIO()
