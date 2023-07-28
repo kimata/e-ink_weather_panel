b344b7868c8be16721fdfbe34a20d8c55894b622
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-07-28 09:29:10 +0900

    fix: bug

--- src/healthz.py
+++ src/healthz.py
@@ -1,30 +1,66 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Liveness のチェックを行います
+
+Usage:
+  healthz.py [-c CONFIG] [-d]
+
+Options:
+  -c CONFIG         : CONFIG を設定ファイルとして読み込んで実行します．[default: config.yaml]
+  -d                : デバッグモードで動作します．
+"""
+
+from docopt import docopt
+
 import pathlib
 import datetime
 import sys
+import logging
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent / "lib"))
 
 from config import load_config
+import logger
 
 config = load_config()
 
-liveness_file = pathlib.Path(config["LIVENESS"]["FILE"])
+######################################################################
+if __name__ == "__main__":
+    args = docopt(__doc__)
 
-if not liveness_file.exists():
-    print("Not executed.", file=sys.stderr)
-    sys.exit(-1)
+    config_file = args["-c"]
+    debug_mode = args["-d"]
 
-elapsed = datetime.datetime.now() - datetime.datetime.fromtimestamp(
-    liveness_file.stat().st_mtime
-)
-if elapsed.seconds > config["PANEL"]["UPDATE"]["INTERVAL"]:
-    print(
-        "Execution interval is too long. ({elapsed:,} sec)".format(
-            elapsed=elapsed.seconds
-        ),
-        file=sys.stderr,
+    if debug_mode:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    logger.init(
+        "hems.rasp-water",
+        level=log_level,
     )
-    sys.exit(-1)
 
-print("OK.", file=sys.stderr)
-sys.exit(0)
+    logging.info("Using config config: {config_file}".format(config_file=config_file))
+    config = load_config(config_file)
+
+    liveness_file = pathlib.Path(config["LIVENESS"]["FILE"])
+
+    if not liveness_file.exists():
+        logging.warning("Not executed.")
+        sys.exit(-1)
+
+    elapsed = datetime.datetime.now() - datetime.datetime.fromtimestamp(
+        liveness_file.stat().st_mtime
+    )
+    if elapsed.total_seconds() > config["PANEL"]["UPDATE"]["INTERVAL"]:
+        logging.warning(
+            "Execution interval is too long. ({elapsed:,} sec)".format(
+                elapsed=elapsed.seconds
+            )
+        )
+        sys.exit(-1)
+
+    logging.info("OK.")
+    sys.exit(0)
