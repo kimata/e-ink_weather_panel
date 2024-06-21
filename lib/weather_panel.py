f3ee96e2c5eff92c89ac8c6050934aa04aa1c891
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-06-21 23:13:09 +0900

    fix: 値が 0 の場合に，小数点表示しないようにした

--- lib/weather_panel.py
+++ lib/weather_panel.py
@@ -204,7 +204,9 @@ def draw_text_info(
     unit_pos_y = pos_y + text_size(img, face["value"], "0")[1] - text_size(img, face["unit"], "℃")[1]
     unit_pos_x = value_pos_x + 5
 
-    if value < 1:
+    if value < 0.01:
+        value_text = "0"
+    elif value < 1:
         value_text = "{value:.1f}".format(value=value)
     else:
         value_text = "{value:.0f}".format(value=value)
